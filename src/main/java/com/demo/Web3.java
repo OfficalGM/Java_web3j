c9b236b2be3e09f67ca2b05f136cf71bc61b481c
tung-lin <gm10285@gmail.com>
2018-11-13 18:07:08 +0800

    find send and sendasync

--- src/main/java/com/demo/Web3.java
+++ src/main/java/com/demo/Web3.java
@@ -14,11 +14,8 @@ import org.web3j.tx.Contract;
 import org.web3j.tx.gas.DefaultGasProvider;
 import org.web3j.utils.Convert;
 import org.web3j.utils.Numeric;
-
-
 import java.io.IOException;
 import java.math.BigInteger;
-
 import static org.web3j.utils.Convert.Unit.ETHER;
 
 
@@ -63,8 +60,6 @@ public class Web3 {
         BigInteger contractGasPrice = DefaultGasProvider.GAS_PRICE;
         try {
             contract = contractFactory.LoadContract(contractName, contractAddress, web3j, credentials, contractGasPrice, contractGasLimit);
-//            contract= Refund.load(contractAddress, web3j, credentials, contractGasPrice, contractGasLimit);
-
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -72,25 +67,11 @@ public class Web3 {
     }
 
     //簽署交易
-    public void SignTransaction() {
-        Credentials credentials = Credentials.create("af58e057cb1ccbcf31bed1dff0a56910e36a6e5b5c2e3e4cdcc742bbac662875");
-//        RawTransactionManager rawTransactionManager=new RawTransactionManager(web3j,credentials);
-//        sendTransaction：数据签名和nonce的计算都由以太坊节点来完成，不需要我们关心
-//        sendRawTransaction：数据签名和nonce需要外部处理好，再发送交易到以太坊节点
-        String address = "0x4E82321967Cb2aF509A7FFf42F771e5Cda08A49c";
-        BigInteger nonce=GetNonce(address);
+    public void SignTransaction(String privatekey,BigInteger nonce,String toAddress) {
+        Credentials credentials = Credentials.create(privatekey);
         BigInteger contractGasLimit = DefaultGasProvider.GAS_LIMIT;
         BigInteger contractGasPrice = DefaultGasProvider.GAS_PRICE;
-        String toaddress="0xf3fbadb5887a21a22215d7a86e8b41d0a6dc1efd";
-//        RawTransaction rawTransaction=RawTransaction.createEtherTransaction(nonce,contractGasPrice,contractGasLimit,toaddress,ConvertToWei("500",ETHER));
-//        byte []signedMessage= TransactionEncoder.signMessage(rawTransaction,credentials);
-//        String hexValue = Numeric.toHexString(signedMessage);
-//        try {
-//            EthSendTransaction ethSendTransaction = web3j.ethSendRawTransaction(hexValue).send();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-        RawTransaction rawTransaction=RawTransaction.createTransaction(nonce,contractGasPrice,contractGasLimit,toaddress,ConvertToWei("10",ETHER),"pay");
+        RawTransaction rawTransaction=RawTransaction.createTransaction(nonce,contractGasPrice,contractGasLimit,toAddress,ConvertToWei("5",ETHER),"pay");
         byte []signedMessage= TransactionEncoder.signMessage(rawTransaction,credentials);
         String hexValue = Numeric.toHexString(signedMessage);
         try {
@@ -102,7 +83,7 @@ public class Web3 {
 
 
     }
-    private BigInteger GetNonce(String address){
+    public BigInteger GetNonce(String address){
         EthGetTransactionCount ethGetTransactionCount = null;
         try {
             ethGetTransactionCount = web3j.ethGetTransactionCount(
--- src/main/java/com/demo/demo.java
+++ src/main/java/com/demo/demo.java
@@ -9,17 +9,20 @@ import static org.web3j.utils.Convert.Unit.ETHER;
 public class demo {
 
     public static void main(String args[]) throws Exception {
-        Web3 web3=new Web3();
+        String url="https://ropsten.infura.io/v3/";
+        String privatekey="";
+        Web3 web3=new Web3(url);
 //        web3.GetMiningStatus();
-        String contractAddress="0xf3fbadb5887a21a22215d7a86e8b41d0a6dc1efd";
+        String contractAddress="0xd44af5c682157f493ba2ebede6e0506428debf67";
         String contractName="Refund";
         System.out.println(web3.GetMiningStatus());
         System.out.println(web3.ConvertToWei("1", ETHER));
         System.out.println(web3.ConvertFromWei("5000000000000000000",ETHER));
         Contract refund=web3.LoadContract(contractName,contractAddress);
 //        System.out.println(((Refund)refund));
-        web3.SignTransaction();
-        System.out.println(((Refund)refund).getBalance().send());
+//        web3.SignTransaction(privatekey,web3.GetNonce("0xc222fDe7CaE05d8514DF13C901e4BEa3F23523cf"),contractAddress);
+        ((Refund)refund).refundBalance().send();
+//        System.out.println(((Refund)refund).getBalance().send());
 //        web3.SignTransaction();
     }
 
